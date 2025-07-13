d9bd49f7d206e824202b69a974d3393d37554cc1
Sujal8182 <sujalsoni8182@gmail.com>
2025-07-14 04:02:30 +0530

    mobile local complete

--- js/mobile.js
+++ js/mobile.js
@@ -19,7 +19,7 @@ let key = 0
       const card = document.createElement("a");
       card.className =
       "bg-white w-full sm:h-[320px] h-auto flex flex-col sm:flex-row p-4 border border-gray-200 cursor-pointer shadow-sm hover:shadow-md transition";
-      // card.setAttribute("href","#")
+      card.setAttribute("href","bhavdeep.html")
       card.setAttribute("onclick",`local(${key})`)
     card.innerHTML = `
       <!-- Image Slider -->
@@ -73,23 +73,22 @@ let key = 0
     .then((res)=> res.json())
     .then((data)=> {
       localStorage.clear()
-        localStorage.setItem("brand", data[key].brand);
+      localStorage.setItem("brand", data[key].brand);
       localStorage.setItem("model", data[key].model);
       localStorage.setItem("desp", data[key].description);
       localStorage.setItem("price", data[key].price);
-      localStorage.setItem("image", data[key].images[0]);
-
+      localStorage.setItem("image", data[key].image[0]);
       localStorage.setItem("processor", data[key].specifications.processor);
       localStorage.setItem("ram", data[key].specifications.ram);
       localStorage.setItem("storage", data[key].specifications.storage);
       localStorage.setItem("display", data[key].specifications.display);
-      localStorage.setItem("battery", specifications.battery);
+      localStorage.setItem("battery", data[key].specifications.battery);
       localStorage.setItem("rearcamera", data[key].specifications.rear_camera);
       localStorage.setItem("frontcamera", data[key].specifications.front_camera);
       localStorage.setItem("os", data[key].specifications.os);
       localStorage.setItem("connectivity", data[key].specifications.connectivity);
 
-  });
+  }); 
 } 
   loadProducts();
 
