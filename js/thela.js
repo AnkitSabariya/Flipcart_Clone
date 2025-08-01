63897893c24b230f4e95a1192c5e9528f1672ea9
Mr Ankit <ankitsabariya@gmail.com>
2025-08-01 11:53:20 +0530

    Website all page and all done

--- js/thela.js
+++ js/thela.js
@@ -1,17 +1,53 @@
-fetch('thela.json')
-  .then(res => res.json())
-  .then(data => {
-    const row = document.getElementById('productRow');
-    data.forEach(product => {
-      row.innerHTML += `
-        <div class="product-box">
-          <img src="${product.image[0]}" alt="${product.brand}">
-          <div class="product-title">${product.brand} - ${product.model}</div>
-          <div class="product-rating">${product.category} • ${product.fabric}</div>
-          <div class="product-price">₹${product.price_usd}</div>
-          <div class="product-offer">${product.occasion}</div>
-        </div>
-      `;
-    });
-  })
-  .catch(err => console.error("JSON load error:", err));
+const cardBox = document.querySelector(".cardbox");
+let key = 0
+    function renderCards() {
+        fetch("../json/thela.json")
+        .then((res)=> res.json())
+        .then((data)=>{
+
+            cardBox.innerHTML = "";
+      
+            data.forEach((product) => {
+              const {
+                brand,
+                model,
+                category,
+                type,
+                dial_shape,
+                strap_material,
+                water_resistant,
+                color,
+                price_usd,
+                image,
+                short_description,
+                id
+              } = product;
+      
+              const price_inr = Math.round(price_usd * 83); // USD to INR approx
+      
+              const card = document.createElement("a");
+              card.className = "max-w-[350px] bg-white flex flex-col  rounded-lg overflow-hidden shadow hover:shadow-lg transition-all duration-300 group cursor-pointer";
+               card.setAttribute("href", `product.html?id=${id}&json=thela`);
+              card.innerHTML = `
+                            <!-- Image Section -->
+              <div class="relative w-full h-[76%] overflow-hidden">
+                  <img src="${image}" 
+                      alt="${model}"
+                      class="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105" />
+              </div>
+
+              <!-- Product Info -->
+              <div class="p-3 space-y-1 text-center">
+                  <h2 class="text-sm font-semibold text-gray-800">${brand} </h2>
+                  <p class="text-sm text-gray-700">${model} ${type} ${dial_shape} ${strap_material}</p>
+                  <div class="text-base font-bold text-gray-900 mt-1">₹<span id="price">${Math.round(price_usd * 83)}</span></div>
+              </div>
+              `;
+      
+              cardBox.appendChild(card);
+              key++
+            });
+        })
+    }
+
+    renderCards();
